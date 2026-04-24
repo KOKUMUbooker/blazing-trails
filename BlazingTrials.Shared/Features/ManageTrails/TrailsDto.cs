53de4377cd34f3c2241693014ab41138afc06cef
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-24 08:45:14 +0300

    Add shared library

---
+++ BlazingTrials.Shared/Features/ManageTrails/TrailsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazingTrials.Shared.Features.ManageTrails;
+
+public class TrailDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Location { get; set; } = "";
+    public int TimeInMinutes { get; set; }
+    public int Length { get; set; }
+    public List<RouteInstruction> Route { get; set; } = new List<RouteInstruction>();
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; } = "";
+    }
+}
