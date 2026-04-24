98af1d375c655fc5fc316655d7307d9fb9ac8983
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-24 17:10:47 +0300

    Add validation to the AddTrails page

--- BlazingTrails.Api/obj/Debug/net10.0/BlazingTrails.Api.AssemblyInfo.cs
+++ BlazingTrails.Api/obj/Debug/net10.0/BlazingTrails.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingTrails.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf97c68ab38a1808e7fcbac8002c26398d1faa1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ff811881ef3a85ab0fd6f218e9493f35219725")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingTrails.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingTrails.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
